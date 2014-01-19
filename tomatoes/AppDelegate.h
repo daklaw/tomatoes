d363556900b41ad33c339f6ce9f47f5896e5263e
David Law <davidlaw@61.87.208.web-pass.com>
2014-01-19 00:12:21 -0800

    Initial Commit

---
+++ tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatoes
+//
+//  Created by David Law on 1/19/14.
+//  Copyright (c) 2014 David Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tomatoes
+//
+//  Created by David Law on 1/19/14.
+//  Copyright (c) 2014 David Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
